aad6b0c5911dfc3a7c9ce2a3569f4a3709e05326
weixiaoguo <xiao66guo@163.com>
2018-10-28 14:28:52 +0800

    flask_session和数据库迁移的集成

--- manage.py
+++ manage.py
@@ -2,10 +2,12 @@
 __author__ = 'xiaoguo'
 
 
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 import redis
 from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
+from flask_migrate import MigrateCommand, Migrate, Manager
 
 
 # 当前应用程序的配置类
@@ -22,6 +24,15 @@ class Config(object):
     REDIS_HOST = '127.0.0.1'
     REDIS_PORT = 6379
 
+    # 设置session保存的参数
+    SESSION_TYPE = 'redis'
+    # 设置session保存redis的相关链接信息，如果不进行设置会以默认的IP和端口进行处理
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 开启session签名
+    SESSION_USE_SIGNER = True
+    # 设置session的过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
+
 
 app = Flask(__name__)
 
@@ -33,13 +44,21 @@ db = SQLAlchemy(app)
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 # 开启CSRF保护
 CSRFProtect(app)
+# 指定session的保存位置
+Session(app)
+# 创建一个manager对象
+manager = Manager(app)
+# 将APP与数据库db进行关联
+Migrate(app, db)
+# 添加迁移命令
+manager.add_command('db', MigrateCommand)
 
 
 @app.route('/',methods=['get', 'post'])
 def index():
-
+    session['name'] = 'xiaohau'
     return 'index222'
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
