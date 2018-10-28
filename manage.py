07a969de74152f637907ec51148698420b8a4baa
weixiaoguo <xiao66guo@163.com>
2018-10-28 11:20:37 +0800

    添加配置类

--- manage.py
+++ manage.py
@@ -1,19 +1,23 @@
 # -*- coding:utf-8 -*-
 __author__ = 'xiaoguo'
 
-
 from flask import Flask
 
+
+# 当前应用程序的配置类
+class Config(object):
+    DEBUG = True
+
+
 app = Flask(__name__)
+# 从对象中加载应用程序的配置
+app.config.from_object(Config)
 
 
 @app.route('/')
 def index():
+    return 'index222'
 
-    return 'index'
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
+    app.run()
