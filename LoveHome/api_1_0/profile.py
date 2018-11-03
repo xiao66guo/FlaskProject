3e31507a75c9ccf63e6b2530eb310238018cb8e5
weixiaoguo <xiao66guo@163.com>
2018-11-03 19:38:07 +0800

    用户头像上传后端的实现

--- LoveHome/api_1_0/profile.py
+++ LoveHome/api_1_0/profile.py
@@ -2,11 +2,58 @@
 __author__ = 'xiaoguo'
 
 from . import api
-from flask import session, current_app, jsonify
+from flask import session, current_app, jsonify, request
 from LoveHome.models import User
 from LoveHome.utils.response_code import RET
+from LoveHome.utils.image_storage import upload_image
+from LoveHome import db, constants
 
 
+'''上传用户图像'''
+@api.route('/user/head_image', methods=['POST'])
+def upload_userImage():
+    # 1、获取到上传的头像
+    try:
+        avatar_data = request.files.get('avatar').read()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DATAERR, errmsg='读取文件失败')
+
+    # 3、上传图片文件到七牛云
+    try:
+        key = upload_image(avatar_data)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg='上传图片失败')
+
+    # 4、如果上传成功，将头像保存到用户表中的头像字段
+    user_id = session.get('user_id')
+    try:
+        user = User.query.get(user_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='查询数据失败')
+
+    if not user:
+        return jsonify(errno=RET.NODATA, errmsg='用户不存在')
+
+    # 赋值到用户模型
+    user.avatar_url = key
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg='保存数据失败')
+
+    # 5、返回响应，附带头像地址
+    avatar_url = constants.QINIU_DOMIN_PREFIX + key
+    return jsonify(errno=RET.OK, errmsg='上传成功', data={'avatar_url': avatar_url})
+
+
+
+
+'''获取用户信息'''
 @api.route('/user')
 def get_user_info():
 
