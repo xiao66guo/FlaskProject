9ea63d4f87789b8ef88c4996071cacae7bc9e791
weixiaoguo <xiao66guo@163.com>
2018-11-03 13:32:42 +0800

    登录功能的前后端实现

--- LoveHome/api_1_0/passport.py
+++ LoveHome/api_1_0/passport.py
@@ -4,10 +4,50 @@ __author__ = 'xiaoguo'
 
 '''实现登录和注册'''
 from . import api
-from flask import request, jsonify, current_app
+from flask import request, jsonify, current_app, session
 from LoveHome.utils.response_code import RET
 from LoveHome import redis_store, db
 from LoveHome.models import User
+import re
+
+
+@api.route('/session', methods=['POST'])
+def login():
+    # 1、获取参数
+    data_dict = request.json
+    mobile = data_dict.get('mobile')
+    password = data_dict.get('password')
+    print mobile
+
+    # 2、校验参数
+    if not all([mobile, password]):
+        return jsonify(errno=RET.PARAMERR, errmsg='参数错误')
+    # 校验手机号是否正确
+    if not re.match('^1[34578][0-9]{9}', mobile):
+        return jsonify(errno=RET.PARAMERR, errmsg='请输入正确的手机号')
+
+    # 3、通过参数mobile查询指定的用户
+    try:
+        user = User.query.filter(User.mobile == mobile).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='数据查询失败')
+
+    if not user:
+        return jsonify(errno=RET.USERERR, errmsg='当前用户不存在')
+
+    # 4、校验密码
+    if not user.check_password(password):
+        return jsonify(errno=RET.PWDERR, errmsg='密码错误')
+
+    # 5、保存用户信息到 session 中
+    session['user_id'] = user.id
+    session['name'] = user.name
+    session['mobile'] = user.mobile
+
+    # 6、给出响应
+    return jsonify(errno=RET.OK, errmsg='登录成功')
+
 
 
 @api.route('/users', methods=['POST'])
--- LoveHome/models.py
+++ LoveHome/models.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-#ihome所使用的所有模型
+#LoveHome所使用的所有模型
 
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -36,6 +36,9 @@ class User(BaseModel, db.Model):
     @password.setter
     def password(self, value):
         self.password_hash = generate_password_hash(value)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Area(BaseModel, db.Model):
     """城区"""
