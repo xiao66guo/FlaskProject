2423a3eadfcbbe025801e2ce50bd8b32d9dddd67
weixiaoguo <xiao66guo@163.com>
2018-11-05 21:23:22 +0800

    加载发布新房屋的城区信息

--- LoveHome/static/js/ihome/newhouse.js
+++ LoveHome/static/js/ihome/newhouse.js
@@ -7,7 +7,19 @@ $(document).ready(function(){
     // $('.popup_con').fadeIn('fast');
     // $('.popup_con').fadeOut('fast');
 
-    // TODO: 在页面加载完毕之后获取区域信息
+    // 在页面加载完毕之后获取区域信息
+    $.get('/api/v1.0/areas', function (resp) {
+        if (resp.errno == '0'){
+            for (var i=0; i<resp.data.length; i++){
+                var aid = resp.data[i].aid
+                var aname = resp.data[i].aname
+                $('#area-id').append('<option value="' + aid + '">'+ aname +'</option>')
+            }
+
+        }else{
+            alert(resp.errmsg)
+        }
+    })
 
     // TODO: 处理房屋基本信息提交的表单数据
 
