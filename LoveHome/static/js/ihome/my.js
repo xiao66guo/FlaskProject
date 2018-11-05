e39a042e43c8c2498b544d04a092a5aa05a80e2a
weixiaoguo <xiao66guo@163.com>
2018-11-05 15:16:36 +0800

    实现用户中心页面个人信息显示功能

--- LoveHome/static/js/ihome/my.js
+++ LoveHome/static/js/ihome/my.js
@@ -10,6 +10,16 @@ function logout() {
 
 $(document).ready(function(){
 
-    // TODO: 在页面加载完毕之后去加载个人信息
+    // 在页面加载完毕向后端查询用户的信息
+    $.get('/api/v1.0/user', function (resp) {
+        if (resp.errno == '0') {
+            // 获取用户头像
+            $('#user-avatar').attr('src', resp.data.avatar_url)
 
-});
+            $('#user-name').html(resp.data.name)
+            $('#user-mobile').html(resp.data.mobile)
+        } else {
+            alert(resp.errmsg)
+        }
+    })
+})
